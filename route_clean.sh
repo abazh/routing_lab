54cdb5d6b9b0f5e9c2387dfd3eb143b3f91bc7ed
Achmad Basuki <9971912+abazh@users.noreply.github.com>
2020-10-27 14:08:14 +0700

    Update route_clean.sh

--- route_clean.sh
+++ route_clean.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-for i in 0 1 2 3 4; # Change default gateway to each router
+for i in 1 2 5 6 9 10; # Change default gateway to each router
 do 
-  docker exec -it C$i /sbin/ip route change default via 10.10.$i.254; 
+  if docker exec -it C$i /sbin/ip route change default via 10.10.$i.254;
+  then echo "Changing Gateway at C$i is successful"
+  else echo "Changing Gateway at C$i is failed"
+  fi
 done
 
-for i in 1 2 3 4; # Delete default gateway on each router
+for i in 1 2 3 4 5 6; # Delete default gateway on each router
 do 
-  docker exec -it R$i /sbin/ip route delete default; 
+  if docker exec -it R$i /sbin/ip route delete default;
+  then echo "Removing Gateway at R$i is successful"
+  else echo "Removing Gateway at R$i is failed"
+  fi 
 done
 
 for i in 1 2 3; # Delete default gateway on each border router
 do 
-  docker exec -it BR0$i /sbin/ip route delete default; 
+  if docker exec -it BR0$i /sbin/ip route delete default;
+  then echo "Removing Gateway at BR0$i is successful"
+  else echo "Removing Gateway at BR0$i is failed"
+  fi 
 done
