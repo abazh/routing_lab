2d2b7a18fd20e2d209c5481c9da00c648b19e576
Achmad Basuki <9971912+abazh@users.noreply.github.com>
2021-06-04 16:59:04 +0700

    update ipv6 address and route_clean script

--- route_clean.sh
+++ route_clean.sh
@@ -2,6 +2,7 @@
 for i in 1 2 5 6 9 10; # Change default gateway to each router
 do 
   if docker exec -it C$i /sbin/ip route change default via 10.10.$i.254;
+     docker exec -it C$i /sbin/ip -6 route change default via fc00:10:10:$i::254;
   then echo "Changing Gateway at C$i is successful"
   else echo "Changing Gateway at C$i is failed"
   fi
@@ -10,6 +11,7 @@ done
 for i in 1 2 3 4 5 6; # Delete default gateway on each router
 do 
   if docker exec -it R$i /sbin/ip route delete default;
+     docker exec -it R$i /sbin/ip -6 route delete default;
   then echo "Removing Gateway at R$i is successful"
   else echo "Removing Gateway at R$i is failed"
   fi 
@@ -18,6 +20,7 @@ done
 for i in 1 2 3; # Delete default gateway on each border router
 do 
   if docker exec -it BR0$i /sbin/ip route delete default;
+     docker exec -it BR0$i /sbin/ip -6 route delete default;
   then echo "Removing Gateway at BR0$i is successful"
   else echo "Removing Gateway at BR0$i is failed"
   fi 
